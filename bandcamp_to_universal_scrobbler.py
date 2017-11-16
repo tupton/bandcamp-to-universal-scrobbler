89dd0c615317be56655c0f8443f346f74c740cbd
Thomas Upton <tupton@datastax.com>
2017-11-16 17:09:31 -0600

    Don't attempt to parse empty time strings

--- bandcamp_to_universal_scrobbler.py
+++ bandcamp_to_universal_scrobbler.py
@@ -20,6 +20,8 @@ import re
 __version__ = "0.1.0"
 
 def time_to_seconds(t):
+    if not t:
+        return 0
     convert = [3600, 60, 1]
     split = t.split(":")
     return sum(c * t for c, t in zip(convert[-1 * len(split):], map(int, split)))
